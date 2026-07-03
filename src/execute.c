5daa642fcfd2fbca128c0d0038793e3b4d31e7fa
yakishamo <82935423+yakishamo@users.noreply.github.com>
2026-07-03 16:26:39 +0900

    argv処理修正

--- src/execute.c
+++ src/execute.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
 #include "execute.h"
 #include "parse.h"
 
+int cd(char *path) {
+  if(!path) {
+    path = getenv("HOME");
+    if(!path) {
+      fprintf(stderr, "cd: $HOME not set");
+      return 0;
+    }
+  }
+  if (chdir(path) != 0) {
+    perror("cd");
+  }
+  return 0;
+}
+
 int execute_command(Command *com) {
   char **argv = malloc(sizeof(char*) * com->size);
 
-  for(int i = 0; i < com->size; i++) {
+  int i = 0;
+  while(1) {
     argv[i] = next_token(com);
     if(!argv[i]) {
       break;
     }
+    i++;
+  }
+
+  if(strcmp(argv[0], "cd") == 0){ 
+    return cd(argv[1]);
   }
 
   pid_t pid = fork();
